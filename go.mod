17ff57b95e0faaa6494ead6c36f01ad4282a572b
Matthew B. Gray <hey@matthew.nz>
2021-01-01 10:06:43 +1300

    WIP, segfaults when fsevent fires

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/heymatthew/tcr
 
 go 1.15
+
+require github.com/fsnotify/fsnotify v1.4.9
--- main.go
+++ main.go
@@ -2,23 +2,93 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"io/fs"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
+	"time"
+
+	"github.com/fsnotify/fsnotify"
 )
 
+var watcher *fsnotify.Watcher
+
 func main() {
+	// creates a new file watcher
+	watcher, _ = fsnotify.NewWatcher()
+	defer watcher.Close()
+
+	// starting at the root of the project, walk each file/directory searching for directories
+	err := filepath.WalkDir(".", watchDir)
+	if err != nil {
+		fmt.Println("ERROR", err)
+	}
+
+	done := make(chan bool)
+
 	testCommand := flag.String("c", "go test", "test command")
 	flag.Parse()
-	err := run(*testCommand)
-	if err != nil {
-		run("git checkout .")
-	} else {
-		run("git add .")
+
+	// https://medium.com/@skdomino/watch-this-file-watching-in-go-5b5a247cf71f
+	go func() {
+		for {
+			output := make(chan fsnotify.Event, 1)
+			go debounce(500*time.Millisecond, watcher.Events, output)
+
+			select {
+			case event := <-output:
+				fmt.Printf(" - %s - ", event)
+				if strings.HasPrefix(event.Name, ".git") {
+					continue
+				}
+				err := run(*testCommand)
+				if err != nil {
+					run("git checkout .")
+				} else {
+					run("git add .")
+				}
+			case err := <-watcher.Errors:
+				fmt.Println("ERROR", err)
+			}
+		}
+	}()
+
+	<-done
+}
+
+func debounce(interval time.Duration, in chan fsnotify.Event, out chan fsnotify.Event) {
+	var item fsnotify.Event
+	timer := time.NewTimer(interval)
+	for {
+		select {
+		case item = <-in:
+			timer.Reset(interval)
+		case <-timer.C:
+			out <- item
+		}
 	}
 }
 
+// watchDir gets run as a walk func, searching for directories to add watchers to
+func watchDir(path string, d fs.DirEntry, err error) error {
+	if err != nil {
+		fmt.Printf("Error walking %s: %s\n", path, err)
+		return err
+	}
+
+	// since fsnotify can watch all the files in a directory, watchers only need
+	// to be added to each nested directory
+	if d.IsDir() {
+		watcher.Add(path)
+	}
+
+	return nil
+}
+
 func run(cmd string) error {
+	fmt.Printf("\n> %s\n", cmd)
 	runnable := strings.Split(cmd, " ")
 	runner := exec.Command(runnable[0], runnable[1:]...)
 	runner.Stdout = os.Stdout
