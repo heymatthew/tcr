19906bcf7915de313325bb01584f467874237315
Matthew B. Gray <code@matthew.nz>
2021-10-09 17:48:05 +1300

    Clear screen between runs

--- main.go
+++ main.go
@@ -2,14 +2,19 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
 	"os/exec"
 	"strings"
 )
 
+var clearScreen = "\033c"
+
 func main() {
 	testCommand := flag.String("c", "go test", "test command")
 	flag.Parse()
+	fmt.Printf(clearScreen)
+
 	err := run(*testCommand)
 	if err != nil {
 		run("git checkout .")
@@ -19,6 +24,7 @@ func main() {
 }
 
 func run(cmd string) error {
+	fmt.Printf("\n> %s\n", cmd)
 	runnable := strings.Split(cmd, " ")
 	runner := exec.Command(runnable[0], runnable[1:]...)
 	runner.Stdout = os.Stdout
